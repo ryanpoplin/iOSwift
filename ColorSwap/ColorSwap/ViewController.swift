ef6b9fa24181f498fb579a655a5bb7820205ab70
Byrdann Fox <mobileupstate@gmail.com>
2015-01-23 02:28:45 -0500

    ColorSwap app done, just need to utilize Auto Layout and run throught the Delegate...

--- ColorSwap/ColorSwap/ViewController.swift
+++ ColorSwap/ColorSwap/ViewController.swift
@@ -10,16 +10,52 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    @IBOutlet weak var colorLabel: UILabel!
+    @IBOutlet weak var colorTextLabel: UILabel!
     
     @IBOutlet weak var redButton: UIButton!
+    
     @IBOutlet weak var greenButton: UIButton!
+    
     @IBOutlet weak var blueButton: UIButton!
+    
     @IBOutlet weak var yellowButton: UIButton!
     
     @IBAction func colorButtonPressed(sender: UIButton) {
     
+        let buttonTitle = sender.titleForState(.Normal)!
         
+        let colorLabelText = "\(buttonTitle)"
+        
+        redButton.exclusiveTouch = true
+        greenButton.exclusiveTouch = true
+        blueButton.exclusiveTouch = true
+        yellowButton.exclusiveTouch = true
+        
+        colorTextLabel.text = colorLabelText
+        
+        switch buttonTitle {
+            
+        case "RED":
+            
+            self.view.backgroundColor = UIColor.redColor()
+            
+        case "GREEN":
+            
+            self.view.backgroundColor = UIColor.greenColor()
+            
+        case "BLUE":
+            
+            self.view.backgroundColor = UIColor.blueColor()
+            
+        case "YELLOW":
+            
+            self.view.backgroundColor = UIColor.yellowColor()
+            
+        default:
+            
+            println("...")
+            
+        }
     
     }
     
