67e6f41226f32ac20cd50e5277111915293e3576
Byrdann Fox <mobileupstate@gmail.com>
2015-01-24 05:58:32 -0500

    Updates...

--- ColorSwap/ColorSwap/ViewController.swift
+++ ColorSwap/ColorSwap/ViewController.swift
@@ -11,7 +11,9 @@ import UIKit
 class ViewController: UIViewController {
     
     override func prefersStatusBarHidden() -> Bool {
+    
         return true
+    
     }
     
     @IBOutlet weak var redButton: UIButton!
