3b31a9330d7b23e72dd7e17954b9803fb3d1a8dc
Byrdann Fox <mobileupstate@gmail.com>
2015-01-24 14:50:42 -0500

    Objective-C Quiz App...

---
+++ TestQuiz/TestQuiz/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TestQuiz
+//
+//  Created by Byrdann Fox on 1/24/15.
+//  Copyright (c) 2015 Byrdann Fox Solutions, Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TestQuiz/TestQuiz/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TestQuiz
+//
+//  Created by Byrdann Fox on 1/24/15.
+//  Copyright (c) 2015 Byrdann Fox Solutions, Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
